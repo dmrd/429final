b419aca4917a1fb19f1d44143e7add6c26dd1a28
David Dohan <git@ddohan.com>
2015-01-10 19:13:23 -0500

    Export trainingImageLabeler files to VOC xml

---
+++ src/classify/labelsToVOC.m
@@ -0,0 +1,58 @@
+function x = labelsToVOC(outDir, classNames, varargin)
+%%
+% Given multiple arguments exported from trainingImageLabeler and the names, combine the
+% bounding boxes for use in GetNegativeSamples and export to VOC2012 format
+% Example usage: labelsToVOC('./xml/', {'dilbert', 'dogbert'}, dilbert, dogbert)
+images = [];
+for i = 1:length(varargin)
+    imNames = extractfield(varargin{i}, 'imageFilename');
+    images = [images, imNames];
+end
+images = sort(unique(images));
+x = images;
+%return
+n = length(images);
+for imId = 1:n
+    %%
+    % Hacky XML printing
+    [path, imageName, extension] = fileparts(images{imId});
+    fid = fopen(fullfile(outDir, [imageName '.xml']), 'w');
+    fprintf(fid, '<annotation>\n');
+    printField(fid, 'folder', 'VOC2012');
+    printField(fid, 'filename', [imageName extension]);
+    im = imread(images{imId});
+    fprintf(fid, '<size>\n');
+    printField(fid, 'width', int2str(size(im, 2)));
+    printField(fid, 'height', int2str(size(im, 1)));
+    printField(fid, 'depth', int2str(size(im, 3)));
+    fprintf(fid, '</size>\n');
+    boxCellId = 1;
+    for label = 1:length(varargin)
+        match = arrayfun(@(x)all(strcmp(x.imageFilename, images(imId))),varargin{label});
+        if sum(match) == 1
+            boxes = varargin{label}(match).objectBoundingBoxes;
+            for boxId = 1:size(boxes, 1)
+                fprintf(fid, '<object>\n');
+                printField(fid, 'name', classNames{label});
+                fprintf(fid, '<bndbox>\n');
+                printField(fid, 'xmin', int2str(boxes(boxId, 1)));
+                printField(fid, 'ymin', int2str(boxes(boxId, 2)));
+                printField(fid, 'xmax', int2str(boxes(boxId, 1) + boxes(boxId, 3)));
+                printField(fid, 'ymax', int2str(boxes(boxId, 2) + boxes(boxId, 4)));
+                fprintf(fid, '</bndbox>\n');
+                fprintf(fid, '</object>\n');
+            end
+        end
+    end
+    fprintf(fid, '</annotation>\n');
+end
+
+
+
+
+
+end
+
+function printField(fid, name, value)
+    fprintf(fid, ['<' name '>' value '</' name '>\n']);
+end
